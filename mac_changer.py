16b1f0d924b873e4e739eb1c13d86f9a80886b9d
Techeavy33 <techeavy@outlook.com>
2019-01-16 17:09:01 -0500

    Create mac_changer.py

---
+++ mac_changer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+
+parser.parse_args()
+interface = raw_input("interface > ")
+new_mac = raw_input("new_MAC > ")
+
+print(" [+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
